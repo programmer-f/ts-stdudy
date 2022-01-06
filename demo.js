1a2d58ff5af63dd9b4d6031388abe578911a7330
andy <18291994396@163.com>
2022-01-06 22:30:31 +0800

    对比js和ts

---
+++ demo.js
@@ -0,0 +1,7 @@
+
+
+function demo(data){
+    return Math.sqrt(data.x ** 2 + data.y ** 2);
+}
+
+demo();
---
+++ demo.ts
@@ -0,0 +1,8 @@
+
+type Point = {x: number, y: number}
+
+function tsDemo(data:Point){
+    return Math.sqrt(data.x ** 2 + data.y ** 2);
+}
+
+tsDemo({x:3,y:4});
